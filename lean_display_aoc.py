939edb9e8f06be134a36621c26dbfdb59efe4e47
Alex NOVAC <anovac@bitdefender.com>
2019-07-29 12:32:02 +0000

    config updates

--- lean_display_aoc.py
+++ lean_display_aoc.py
@@ -7,13 +7,12 @@ import matplotlib.pyplot as plt
 import lean_utils as lu
 import lean_params as lp
 
-label = 'day8'
-training_label = 'baseline-Jul26_10-06-31'
-training_epoch = 8
+label = 'day0-7-8'
+training_label = 'baseline0-7-Jul28_14-36-21'
+training_epoch = 4
 
 redlabels_filename = f'./cache/redlabels_day8.pt'
-# probs_filename = f'./cache/probs_{label}_{training_label}_{training_epoch}.pt'
-probs_filename = './cache/probs_day8.pt'
+probs_filename = f'./cache/probs_{label}_{training_label}_{training_epoch}.pt'
 
 device = lu.get_device()
 redlabels = torch.load( redlabels_filename ).to(device)
@@ -23,7 +22,7 @@ fpr, tpr, thresholds = metrics.roc_curve(redlabels.data.cpu().numpy() , probs.da
 print( metrics.auc(fpr, tpr) )
 
 plt.figure()
-plt.scatter( fpr , tpr )
+plt.plot( fpr , tpr )
 plt.xlabel( 'False Positives' )
 plt.ylabel( 'True Positives' )
 plt.show()
